7e0a797deb2e40ddd4fe40178b698159c84fadd8
aaus26480 <160036946+aaus26480@users.noreply.github.com>
2025-07-17 08:30:49 +0700

    Add files via upload
    "Add chanle.py – my first Python script"

---
+++ chanle.py
@@ -0,0 +1,10 @@
+name = input ("Enter your name: ")
+age = input ("Enter your age: ")
+print ("Hello " + name + "! You are " + age + " years old.")
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+print("Thank you for using the program!")
